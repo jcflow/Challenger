45b2786870ec8692dd9c2f91e3060c11146ad805
Juan <im.jhcc@gmail.com>
2020-04-06 04:32:18 -0400

    Creating first version of GraphQL API.

---
+++ API/Controllers/GraphQLController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Queries;
+using GraphQL;
+using GraphQL.Http;
+using GraphQL.Types;
+using Microsoft.AspNetCore.Mvc;
+using Schema;
+
+namespace API.Controllers
+{
+    [Route("graphql")]
+    public class GraphQLController : Controller
+    {
+        private readonly IDocumentExecuter _executer;
+        private readonly ISchema _schema;
+        private readonly IDocumentWriter _writer;
+
+        public GraphQLController(ISchema schema, IDocumentExecuter documentExecuter, IDocumentWriter writer)
+        {
+            _executer = documentExecuter;
+            _schema = schema;
+            _writer = writer;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] GraphQLQuery query)
+        {
+            //var result = await _executer.ExecuteAsync(_ =>
+            //{
+            //    _.Schema = _schema;
+            //    _.Query = query.Query;
+            //    _.Inputs = query.Variables?.ToInputs();
+
+            //}).ConfigureAwait(false);
+
+            //if (result.Errors?.Count > 0)
+            //{
+            //    return Problem(detail: result.Errors.Select(_ => _.Message).FirstOrDefault(), statusCode: 500);
+            //}
+            //return Ok(result.Data);
+
+
+            if (query == null) { throw new ArgumentNullException(nameof(query)); }
+
+            var inputs = query.Variables.ToInputs();
+            var queryToExecute = query.Query;
+
+
+            var executionOptions = new ExecutionOptions { Schema = _schema, Query = queryToExecute, Inputs = inputs, OperationName = query.OperationName };
+
+            var result = await _executer.ExecuteAsync(executionOptions).ConfigureAwait(false);
+
+
+            if (result.Errors?.Count > 0)
+                return BadRequest(result);
+            else
+                return Ok(result);
+
+        }
+    }
+}
---
+++ API/Startup.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Queries;
+using GraphiQl;
+using GraphQl.AspNetCore;
+using GraphQL;
+using GraphQL.Http;
+using GraphQL.Types;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Models;
+using Repository;
+using Repository.EF;
+using Schema;
+using Schema.Types;
+
+namespace API
+{
+    public class Startup
+
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Context
+            services.AddSingleton<ChallengerContext>();
+
+            //Data Repository
+            services.AddTransient<ITournamentRepository, EFTournamentRepository>();
+            services.AddTransient<IBracketRepository, EFBracketRepository>();
+            services.AddTransient<IScoreRepository, EFScoreRepository>();
+            services.AddTransient<ITeamRepository, EFTeamRepository>();
+
+            //GraphQL Document
+            services.AddSingleton<IDocumentWriter, DocumentWriter>();
+            services.AddSingleton<IDocumentExecuter, DocumentExecuter>();
+            services.AddScoped<ISchema, ChallengerSchema>();
+
+            //GraphQL Query
+            services.AddScoped<ChallengerQuery>();
+
+            //GraphQL Types
+            services.AddSingleton<TournamentType>();
+            services.AddSingleton<BracketType>();
+            services.AddSingleton<ScoreType>();
+            services.AddSingleton<TeamType>();
+
+            services.AddScoped<IDependencyResolver>(_ => new FuncDependencyResolver(_.GetRequiredService));
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
